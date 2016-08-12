e747764bec4bb8b364ca8f19eaf53776e4807112
Charles Huet <Charles.Huet@fei.com>
2016-08-12 15:42:14 +0200

    updated test list

--- Source/cmLocalFastbuildGenerator.h
+++ Source/cmLocalFastbuildGenerator.h
@@ -32,7 +32,7 @@ class cmLocalFastbuildGenerator
 {
 public:
   
-	cmLocalFastbuildGenerator();
+	cmLocalFastbuildGenerator(cmGlobalGenerator *gg, cmMakefile *makefile);
 	virtual ~cmLocalFastbuildGenerator();
 
 	virtual void Generate();
@@ -45,7 +45,7 @@ public:
 		std::map<cmSourceFile const*, std::string>& mapping,
 		cmGeneratorTarget const* gt);
 	virtual std::string GetTargetDirectory(
-		cmTarget const& target) const;
+        cmGeneratorTarget const* target) const;
 	virtual void AppendFlagEscape(std::string& flags,
 		const std::string& rawFlag);
 
